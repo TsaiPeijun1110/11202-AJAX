18f407f7354813ae31aff7a749efa65bc7526501
tsai <lovez81427@gmail.com>
2024-01-16 10:52:29 +0800

    在main新增最新消息區

--- front/main.php
+++ front/main.php
@@ -240,6 +240,8 @@
     </div>
   </div>
 
+  
+
   <div>
     <div style="width:400px; height:487px; margin:0px;border: #EE7728 double 3px;" class="dbor">
       <span class="t ">校園映象區</span>
@@ -283,6 +285,58 @@
     </div>
   </div>
 
+  
+<div>
+  <div style="width: 400px;
+             padding: 2px;
+             height: 486px;
+             margin-top: 10px;
+             padding: 5px 10px 5px 10px;
+             border: #0C3 dashed 3px;
+             position: relative;
+             margin-left: 1211px;
+             margin-top: -486px; ">
+
+        <span class="t botli">最新消息區
+            <?php
+                if($News->count(['sh'=>1])>5){
+                    echo "<a href='?do=news' style='float:right'>More...</a>";
+                }
+            ?>      
+        </span>
+        <ul class="ssaa" style="list-style-type:decimal;">
+        <?php
+            $news=$News->all(['sh'=>1],' limit 5');
+            foreach($news as $n){
+                echo "<li>";
+                echo mb_substr($n['text'],0,20);
+                echo "<div class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</div>";
+                echo "...</li>";
+            }
+        ?>
+        </ul>
+        <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: 
+        rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; 
+        border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">    
+        </div>
+        <script>
+            $(".ssaa li").hover(
+                function() {
+
+                    $("#altt").html("<pre>" +$(this).children(".all").html()  + "</pre>")
+                    $("#altt").show()
+                }
+            )
+            $(".ssaa li").mouseout(
+                function() {
+                    $("#altt").hide()
+                }
+            )
+        </script>
+    </div>
+    </div>
 
 
 
